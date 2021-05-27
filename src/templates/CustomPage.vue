6db36ae789e823574818969ba46b4f2488134e20
O H <olle@unremarkablegarden.com>
2021-05-27 09:56:15 +0200

    new redirect

--- src/templates/CustomPage.vue
+++ src/templates/CustomPage.vue
@@ -1,56 +1,28 @@
 <template lang="pug">
   layout
     .columns
-      //- .column.is-6.desktop.no-pad.gallery-column
-      //-   .gallery
-      //-     .item(v-if='page.image', :style="'background-image: url('+page.image.url+')'")
-      //-     .item(v-else) Add a featured image to this page in Prismic
-      //-     //- prismic-image(:field='page.image', v-if='page.image')
-
-      //- .column.is-6.left
-        .page-wrapper(v-if='page')
-          //- prismic-rich-text(:field='page.title').title
-          .title {{ page.title }}
-          //- prismic-rich-text(:field='page.subtitle', v-if='page.subtitle').subtitle
-          .content
-            prismic-rich-text(:field='page.body')
-
-          //- Newsletter
-
 </template>
 
 
 <style lang="scss">
-// $green: #11ff36;
 $green: rgb(17,230,54);
 </style>
 
 <script>
-// import Newsletter from '~/components/Newsletter.vue'
-
 export default {
-  components: {
-    // Newsletter
-  },
   name: 'Page',
   metaInfo() {
     return {
-      title: 'Coming soon'
-    }
-  },
-  data () {
-    return {
-      gallery: false
+      title: 'Redirecting...'
     }
   },
   created () {
     if (!process.isClient) return
-    window.location.replace("/en/chapter2");
+    
+    // depuislacendredesforets.com
+    const en = 'https://www.fromtheforestsashes.com/'
+    
+    window.location.replace(en);
   },
-  computed: {
-    page () {
-      // return this.$context.node
-    }
-  }
 }
 </script>
