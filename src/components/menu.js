e2b78e6cdec4d7b3527c3d70a2cfc755a92a93df
O H <olle@unremarkablegarden.com>
2021-07-01 14:14:22 +0200

    artistic mediation menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -15,7 +15,7 @@ export default
     // { title: ['Online Dialogues', 'Dialogues en ligne'], to: ['/en/online-dialogues', '/fr/dialogues-en-ligne'] },
     
     // CULTURAL
-    { title: ['Cultural Mediation', 'Médiation Culturelle'], to: ['/en/cultural-mediation', '/fr/mediation-culturelle'] },
+    { title: ['Artistic Mediation', 'Médiation Artistique'], to: ['/en/artistic-mediation', '/fr/mediation-mediation-artistique'] },
   
     
     
