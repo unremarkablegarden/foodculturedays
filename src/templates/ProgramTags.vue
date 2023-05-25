f2aa4e4cd92d60580c6daaf51e82a706ffdb0421
unremarkablegarden <olle@unremarkablegarden.com>
2023-05-25 11:48:14 +0200

    no ranges

--- src/templates/ProgramTags.vue
+++ src/templates/ProgramTags.vue
@@ -717,7 +717,8 @@ export default {
       
       // THIS BROKE THE CODE???
       // turn into ranges
-      dates = this.convertToRanges(dates)
+      // dates = this.convertToRanges(dates)
+      
       // // convert ranges to dates
       // dates = this.convertRangeToDates(dates)
       // debug
