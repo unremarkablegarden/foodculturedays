ef34cc021f2ca58fba36700510d56e75224d6ea5
unremarkablegarden <olle@unremarkablegarden.com>
2024-11-08 15:57:08 +0100

    newsletter

--- src/pages/Newsletter.vue
+++ src/pages/Newsletter.vue
@@ -1,44 +1,46 @@
 <template lang="pug">
   layout
     #newsletter
-      
-      h1 {{ t[lang].title }}
-      .subtitle {{ t[lang].subtitle }}
+      .inner
+        //- h1 {{ t[lang].title }}
+        h1 Newsletter
+        .subtitle Subscribe here
+        //- .subtitle {{ t[lang].subtitle }}
 
-      #mc_embed_signup
-        form#mc-embedded-subscribe-form.validate(action='https://foodculturedays.us16.list-manage.com/subscribe/post?u=07a3eaaaad1fdf2ab12286e6a&id=a741f19be3', method='post', name='mc-embedded-subscribe-form', target='_blank', novalidate)
-          #mc_embed_signup_scroll
-            .mc-field-group
-              label(for='mce-EMAIL') {{ t[lang].form[0] }}
-              input.required.email#mce-EMAIL(type='email', name='EMAIL')
-            .mc-field-group
-              label(for='mce-FNAME') {{ t[lang].form[1] }}
-              input#mce-FNAME(type='text', name='FNAME')
-            .mc-field-group
-              label(for='mce-LNAME') {{ t[lang].form[2] }}
-              input#mce-LNAME(type='text', name='LNAME')
-            .mc-field-group.size1of2
-              label(for='mce-BIRTHDAY-month') {{ t[lang].form[3] }}
-              .datefield
-                span.subfield.monthfield
-                  input.birthday#mce-BIRTHDAY-month(type='text', pattern='[0-9]*', placeholder='MM', size='2', maxlength='2', name='BIRTHDAY[month]')
-                | /
+        #mc_embed_signup
+          form#mc-embedded-subscribe-form.validate(action='https://foodculturedays.us16.list-manage.com/subscribe/post?u=07a3eaaaad1fdf2ab12286e6a&id=a741f19be3', method='post', name='mc-embedded-subscribe-form', target='_blank', novalidate)
+            #mc_embed_signup_scroll
+              .mc-field-group
+                label(for='mce-EMAIL') {{ t[lang].form[0] }}
+                input.required.email#mce-EMAIL(type='email', name='EMAIL')
+              .mc-field-group
+                label(for='mce-FNAME') {{ t[lang].form[1] }}
+                input#mce-FNAME(type='text', name='FNAME')
+              .mc-field-group
+                label(for='mce-LNAME') {{ t[lang].form[2] }}
+                input#mce-LNAME(type='text', name='LNAME')
+              .mc-field-group.size1of2
+                label(for='mce-BIRTHDAY-month') {{ t[lang].form[3] }}
+                .datefield
+                  span.subfield.monthfield
+                    input.birthday#mce-BIRTHDAY-month(type='text', pattern='[0-9]*', placeholder='MM', size='2', maxlength='2', name='BIRTHDAY[month]')
+                  | /
 
-                span.subfield.dayfield
-                  input.birthday#mce-BIRTHDAY-day(type='text', pattern='[0-9]*', placeholder='DD', size='2', maxlength='2', name='BIRTHDAY[day]')
-                span.small-meta.nowrap ( mm / dd )
-            //- #mce-responses.clear
-              .response#mce-error-response(style='display:none')
-              .response#mce-success-response(style='display:none')
+                  span.subfield.dayfield
+                    input.birthday#mce-BIRTHDAY-day(type='text', pattern='[0-9]*', placeholder='DD', size='2', maxlength='2', name='BIRTHDAY[day]')
+                  span.small-meta.nowrap ( mm / dd )
+              //- #mce-responses.clear
+                .response#mce-error-response(style='display:none')
+                .response#mce-success-response(style='display:none')
 
-            div(style='position: absolute; left: -5000px;', aria-hidden='true')
-              input(type='text', name='b_07a3eaaaad1fdf2ab12286e6a_a741f19be3', tabindex='-1')
-            .clear
-              input#mc-embedded-subscribe.button(type='submit', :value='t[lang].form[4]', name='subscribe').submit
+              div(style='position: absolute; left: -5000px;', aria-hidden='true')
+                input(type='text', name='b_07a3eaaaad1fdf2ab12286e6a_a741f19be3', tabindex='-1')
+              .clear
+                input#mc-embedded-subscribe.button(type='submit', :value='t[lang].form[4]', name='subscribe').submit
 
-      //- script(type='text/javascript', src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js')
+        //- script(type='text/javascript', src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js')
 
-      //- script(type='text/javascript') (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);
+        //- script(type='text/javascript') (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);
 
 </template>
 
@@ -105,6 +107,19 @@ export default {
 
 <style lang="scss" scoped>
 #newsletter {
+  // breakpoint for desktop
+  @media (min-width: 768px) {
+    margin: 0 auto;
+    max-width: 32rem;
+    min-height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .inner {
+      border: 2px solid black;
+      padding: 2rem 4rem;
+    }
+  }
   h1 {
     margin-top: 2rem;
   }
@@ -127,6 +142,7 @@ export default {
     border: 1px black solid;
     width: 100%;
     background: white;
+    color: black !important;
     font-family: 'D', -apple-system,system-ui,BlinkMacSystemFont,"Helvetica Neue",Arial,sans-serif;
     // line-height: 1rem;
     // font-size: 0.9rem;
